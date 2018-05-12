bd66b6032c0812d6254d6dd52fea6ab3eb8b1d95
Nick Snyder <n.a.snyder@comcast.net>
2018-05-12 17:38:08 -0400

    Fixed free ship text resizing

--- js/app.js
+++ js/app.js
@@ -1,15 +1,22 @@
+window.addEventListener('resize', resizeFreeShipText);
+document.addEventListener('DOMContentLoaded', resizeFreeShipText);
+
 // Set font size of free ship text
-const text = document.querySelectorAll('.free-ship-text');
-const freeShipDiv = document.querySelector('.free-ship-center');
-const freeShipStyle = getComputedStyle(freeShipDiv);
+function resizeFreeShipText(){
+  const text = document.querySelectorAll('.free-ship-text');
+  const freeShipDiv = document.querySelector('.free-ship-center');
+  const freeShipStyle = getComputedStyle(freeShipDiv);
 
-let divWidth = freeShipDiv.clientWidth;
-divWidth -= parseFloat(freeShipStyle.paddingLeft) + parseFloat(freeShipStyle.paddingRight);
+  let divWidth = freeShipDiv.clientWidth;
+  divWidth -= parseFloat(freeShipStyle.paddingLeft) + parseFloat(freeShipStyle.paddingRight);
 
-text.forEach(line => {
-  const lineStyle = getComputedStyle(line);
-  let lineWidth = line.clientWidth;
-  lineWidth -= parseFloat(lineStyle.paddingLeft) + parseFloat(lineStyle.paddingRight);
-  let newFontSize = (divWidth/lineWidth);
-  line.style.fontSize = newFontSize + 'rem';
-});
+  text.forEach(line => {
+    line.style.fontSize = '1rem';
+    const lineStyle = getComputedStyle(line);
+    let lineWidth = line.clientWidth;
+    lineWidth -= parseFloat(lineStyle.paddingLeft) + parseFloat(lineStyle.paddingRight);
+    let newFontSize = (divWidth/lineWidth);
+    console.log(divWidth, lineWidth);
+    line.style.fontSize = newFontSize + 'rem';
+  });
+}
